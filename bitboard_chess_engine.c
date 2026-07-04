57fa5b90f728b85f6747cf28d00aecfe53061862
Mayank Bhati <bhatimayank987@gmail.com>
2026-07-04 22:54:08 +0530

    pre-calculated knight table complete

--- bitboard_chess_engine.c
+++ bitboard_chess_engine.c
@@ -175,6 +175,10 @@ const U64 not_ab_file = 18229723555195321596ULL;
 /*     PAWN ATTACKS TABLE  [side to move][square]  */
 U64 pawn_attacks[2][64];
 
+
+/*  KNIGHTS ATTACK TABLE [square]*/
+U64 knight_attacks[64];
+
 //generate pawn attacks (curr_square, side to move)
 U64  mask_pawn_attacks(int side, int square){
 
@@ -189,7 +193,8 @@ U64  mask_pawn_attacks(int side, int square){
 
     //white pawns
     if(!side){
-
+        
+        //generate pawn  attacks for white (right shift by offsets)
         if((bitboard >> 7)& not_a_file)
             attacks = attacks | (bitboard >> 7);
 
@@ -200,6 +205,7 @@ U64  mask_pawn_attacks(int side, int square){
     //black pawn
     else{
 
+        //generate pawn  attacks for black(left shift by offsets)
         if((bitboard << 7)& not_h_file)
             attacks = attacks | (bitboard << 7);
 
@@ -208,6 +214,33 @@ U64  mask_pawn_attacks(int side, int square){
     }
 
     //return attack map
+    return attacks;
+}
+
+
+//generate knight attacks 
+U64 mask_knight_attacks(int square){
+
+    //result attack bitboard
+    U64 attacks = 0ULL;
+
+    //piece bitboard
+    U64 bitboard = 0ULL;
+
+    //set pieces on board
+    set_bit(bitboard, square);
+
+    //generate knight attacks --> 17, 15, 6, 10
+    if((bitboard >> 17) & not_h_file ) attacks |= (bitboard >> 17);
+    if((bitboard << 17) & not_a_file ) attacks |= (bitboard << 17);
+    if((bitboard >> 15) & not_a_file ) attacks |= (bitboard >> 15);
+    if((bitboard << 15) & not_h_file ) attacks |= (bitboard << 15);
+    if((bitboard << 06) & not_hg_file) attacks |= (bitboard << 06);
+    if((bitboard >> 06) & not_ab_file) attacks |= (bitboard >> 06);
+    if((bitboard << 10) & not_ab_file) attacks |= (bitboard << 10);
+    if((bitboard >> 10) & not_hg_file) attacks |= (bitboard >> 10);
+
+    //return attacks map
     return attacks;
 }
 
@@ -220,6 +253,9 @@ void init_leapers_attacks(){
         //init pawn attacks
         pawn_attacks[white][square] = mask_pawn_attacks(white, square);
         pawn_attacks[black][square] = mask_pawn_attacks(black, square);
+
+        //init knight attacks
+        knight_attacks[square] = mask_knight_attacks(square);
     }
 }
 
@@ -231,13 +267,15 @@ void init_leapers_attacks(){
 //main driver
 int main(){
 
-    //init leaper peices attacks
+    //init leaper pieces attacks
     
     init_leapers_attacks();
 
     // loop over 64 board squares
     for(int square = 0;  square < 64 ; square++){
-        print_bitboard(pawn_attacks[black][square]);
+        print_bitboard(knight_attacks[square]);
     }
+
+    //print_bitboard(mask_knight_attacks(h5));
     return 0;   
 } 
