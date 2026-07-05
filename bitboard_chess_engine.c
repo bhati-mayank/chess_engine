236dc1bbe308012149a9fcab8421821aba64e94d
Mayank Bhati <bhatimayank987@gmail.com>
2026-07-05 12:51:40 +0530

    added the function to find the index of least significant bit in a bitboard

--- bitboard_chess_engine.c
+++ bitboard_chess_engine.c
@@ -24,14 +24,17 @@ enum {
 //enumeration of pieces
 enum { white, black };
 
-/*"a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"
-"a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"
-"a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"
-"a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"
-"a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"
-"a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"
-"a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"
-"a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"*/
+const char *square_to_coordinates[] = {
+
+    "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+    "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+    "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+    "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+    "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+    "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+    "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+    "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"
+};
 
 
 //get/set/pop macros
@@ -58,6 +61,22 @@ static inline int count_bits(U64 bitboard){
     //return the bit counter 
     return count;
 }
+
+
+//get least significant 1st bit index
+static inline int get_ls1st_bit_index(U64 bitboard){
+
+    //make sure bitboard is not zero
+    if(bitboard){
+        
+        //count trailing bits first
+        return count_bits((bitboard & -bitboard) -1);
+    }
+
+    else 
+        return -1; //as no 1bit present
+}
+
 
 //printing the bitboard
 void print_bitboard(U64 bitboard){
@@ -486,7 +505,14 @@ int main(){
     set_bit(block, b4);
     set_bit(block, g4);
 
-    printf("%d", count_bits(block));
+    print_bitboard(block);
+
+    printf("%d", get_ls1st_bit_index(block));
+
+    printf("\n");
+
+    printf("%s", square_to_coordinates[get_ls1st_bit_index(block)]);
+
 
     return 0;   
 } 
