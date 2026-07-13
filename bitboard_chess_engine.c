cf5d231cbaeade332d62c50fac21d6b5725ac256
bhati_mayank <bhatimayank987@gmail.com>
2026-07-13 10:14:11 +0000

    generated queen attacks table

--- bitboard_chess_engine.c
+++ bitboard_chess_engine.c
@@ -1180,6 +1180,38 @@ static inline U64 get_rook_attacks(int square, U64 occupancy){
     return rook_attacks[square][occupancy];
 }
 
+// get queen attacks 
+static inline U64 get_queen_attacks(int square, U64 occupancy){
+
+    // init result attacks bitboard
+    U64 queen_attacks = 0ULL;
+
+    // init bishop occupancies
+    U64 bishop_occupancy = occupancy;
+
+    // init rook occupancies
+    U64 rook_occupancy = occupancy;
+
+    // get bishop attacks  assuming current board occupancy
+    bishop_occupancy &= bishop_masks[square];
+    bishop_occupancy *= bishop_magic_numbers[square];
+    bishop_occupancy >>= ( 64 - bishop_relevant_bits[square]);
+
+    // get bishop attacks
+    queen_attacks = bishop_attacks[square][bishop_occupancy];
+
+    // get rook attacks  assuming current board occupancy
+    rook_occupancy &= rook_masks[square];
+    rook_occupancy *= rook_magic_numbers[square];
+    rook_occupancy >>= ( 64 - rook_relevant_bits[square]);
+
+    // get rook attacks
+    queen_attacks |= rook_attacks[square][rook_occupancy];
+
+    // return queen attacks
+    return queen_attacks;
+
+}
 
 //======================================//
 /*
@@ -1216,13 +1248,15 @@ int main(){
     //init all
     init_all();
 
-    // parse fen
-    parse_fen(start_position);
+    // init occupancy bit board
+    U64 occupancy = 0ULL;
 
-    //print white occupancies
-    print_bitboard(occupancies[both]);
+    // set occuoancy
+    set_bit(occupancy, b6);
+    set_bit(occupancy, d6);
 
-    print_board();
+    // get queen attacks
+    print_bitboard(get_queen_attacks(d4, occupancy));
     return 0;
 
 } 
